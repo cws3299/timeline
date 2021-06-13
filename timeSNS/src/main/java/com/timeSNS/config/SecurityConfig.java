4bac2a425d32e4a2b0fa3ab90144a80ed8490bdb
jy940408 <junyoung940408@gmail.com>
2021-06-13 17:16:05 +0900

    20210613_timelineSNS_cors_3

--- timeSNS/src/main/java/com/timeSNS/config/SecurityConfig.java
+++ timeSNS/src/main/java/com/timeSNS/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.filter.CorsFilter;
 
 import com.timeSNS.jwt.JwtAccessDeniedHandler;
@@ -70,8 +71,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers("/auth/authenticate").permitAll()
 			.antMatchers("/auth/signup").permitAll()
 			.antMatchers("/static/**").permitAll()
+			.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
 			.anyRequest().authenticated()
 			
+			.and()
+            .cors()
 			.and()
 			.apply(new JwtSecurityConfig(tokenProvider));
 	}
