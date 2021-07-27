2a808f927dd0a2ee5528c10d371a2ba1f57ad066
cws3299 <cws3299@gmail.com>
2021-07-27 16:51:16 +0900

    20210727 1

--- new_frontend/new_frontend/src/components/Post/PostContent.js
+++ new_frontend/new_frontend/src/components/Post/PostContent.js
@@ -1,4 +1,4 @@
-import React, {useState , useEffect , useCallback} from 'react'
+import React, {useState , useEffect , useCallback ,useRef} from 'react'
 import './PostContent.css'
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -8,21 +8,32 @@ import Select from '@material-ui/core/Select';
 import NativeSelect from '@material-ui/core/NativeSelect';
 import { config } from '../../shared/config'
 import axios from 'axios';
+import TextField from '@material-ui/core/TextField';
+
+import DayPickerInput from "react-day-picker/DayPickerInput";
+import MomentLocaleUtils from "react-day-picker/moment";
+import "react-day-picker/lib/style.css";
+import "moment/locale/de";
+import LinesEllipsis from 'react-lines-ellipsis'
 
 const useStyles = makeStyles((theme) => ({
     formControl: {
       margin: theme.spacing(1),
-      minWidth: 980,
-      width:980,
+      minWidth: 900,
+      width:900,
     },
     selectEmpty: {
       marginTop: theme.spacing(2),
     },
   }));
 
+
+
   
 
 function PostContent() {
+    const dateInput = useRef();
+    const placeInput = useRef();
     const [timelines , setTimelines] = useState([])
     const url = config.api
     const _token = localStorage.getItem("token");
@@ -46,34 +57,39 @@ function PostContent() {
       }, []);
 
     const classes = useStyles();
+    const [fileUrl, setFileUrl] = useState(null);
     const [choice_timeline_idx, setChoiceIdx] = React.useState(0)
     const [tlcdate, setTlcdate] = React.useState('')
     const [tlcplace, setTlcplace] = React.useState('')
     const [tlcimage, setTlcimage] = React.useState(null)
     const [tlccontent, setTlccontent] = React.useState('')
-    const [tlcemotion, setTlcemotion] = React.useState('')
-    const [tlcpubyn, setTlcpubyn] = React.useState('')
+    // const [tlcemotion, setTlcemotion] = React.useState('')
+    // const [tlcpubyn, setTlcpubyn] = React.useState('')
     const [tlctag, setTlctag] = React.useState('')
 
 
     const onChange = (e) => {
       setTlcimage(e.target.files[0]);
+      const imageFile = e.target.files[0];
+      const imageUrl = URL.createObjectURL(imageFile);
+      setFileUrl(imageUrl)
     }
   
     const onClick = async () => {
       const formData = new FormData();
-      formData.append('tlcdate', '2021-07-26')
-      formData.append('tlcplace', '전북대')
-      formData.append('tlccontent', '개발은 어렵다')
+      formData.append('tlcdate', tlcdate)
+      formData.append('tlcplace', tlcplace)
+      formData.append('tlccontent', tlccontent)
       // formData.append('tlcemotion', null)
-      formData.append('tlcpubyn', 'y')
-      formData.append('tlctag', '#개발')
+      // formData.append('tlcpubyn', 'y')
+      formData.append('tlctag', tlctag)
       formData.append('tlcimage', tlcimage);
       console.log(formData)
       console.log(tlcimage)
       // 서버의 upload API 호출
       const res = await axios.post(`${url}/post/writepost/${choice_timeline_idx}`, formData, token);
-      console.log('rrrrrrr');
+      console.log(res)
+      console.log(choice_timeline_idx);
     }
 
     const [state, setState] = React.useState('');
@@ -85,8 +101,54 @@ function PostContent() {
       setState(event.target.value)
       console.log(choice_timeline_idx)
     };
+
+    const dateChange = async() =>{
+      await setTlcdate(dateInput.current.state.value)
+      console.log('start',tlcdate)
+      let tl = ''
+      if (tlcdate.length == 8){
+        tl += tlcdate.slice(0,5)
+        tl += '0'
+        tl += tlcdate.slice(5,7)
+        tl += '0'
+        tl += tlcdate.slice(7,9)
+      }else if(tlcdate.length == 9){
+        if (tlcdate[5] === '1'){
+          tl += tlcdate.slice(0,7)
+          tl += '0'
+          tl += tlcdate.slice(7,9)
+        }else{
+          tl += tlcdate.slice(0,5)
+          tl += '0'
+          tl += tlcdate.slice(5,9)
+        }
+      }else{
+        setTlcdate(dateInput.current.state.value)
+      }
+
+      console.log('-----',tl)
+      if (tl.length === 10){
+        setTlcdate(tl)
+      }
+
+      console.log('++',tlcdate)
+
+    }
+
+    const placeChange = (e) =>{
+      setTlcplace(e.target.value)
+    }
+
+    const ContentChange = (e) => {
+      setTlccontent(e.target.value)
+    }
+
+    const TagChange = (e) => {
+      setTlctag(e.target.value)
+    }
     return(
         <div className = "PostContent">
+          <div className="PostContent1">
             <FormControl variant="outlined" className={classes.formControl}>
                 <Select
                 native
@@ -109,8 +171,40 @@ function PostContent() {
                 }
                 </Select>
             </FormControl>
-            <input type="file" onChange={onChange}/>
-            <button onClick={onClick}>제출</button>
+            <button className="PostButton1" onClick={onClick}>제출</button>
+          </div>
+          <div className="PostContent2">
+            <div className = "PostContentContent">
+                <div className = "PostContentDate">
+                  <input className="PostContentPlaceInput" onChange={placeChange} placeholder="장소를 입력해주세요"></input>
+                  <DayPickerInput ref={dateInput} value="DD/MM/YYYY" placeholder="DD/MM/YYYY" format="DD/MM/YYYY"/>
+                  <button className = "PostContentButton" onClick={dateChange}>날짜 확인</button>
+                </div>
+                <div className = "PostContentContentContent">
+                  <textarea className = "PostContentContentContentText" onChange={ContentChange} placeholder="내용을 입력해주세요" />
+                </div>
+                <div className = "PostContentTag">
+                  <input className="PostContentTagInput" onChange={TagChange} placeholder="#태그1#태그2"></input>
+                </div>
+            </div>
+            <div className = "PostContentImage">
+              <div className = "PostContentImage1">
+                <img className = "PostContentImage11" src={fileUrl}>
+                </img>
+              </div>
+              <div className = "PostContentImage2">
+                  {/* <input className = "PostContentImage22" type="file" accept="image/*" onChange={onChange}/> */}
+                  <div className="filebox">
+                    <label for="ex_file">
+                      <div className="fileboxInput">
+                        사진 업로드
+                      </div>
+                    </label>
+                    <input type="file" id="ex_file" accept="image/*" onChange={onChange}/>
+                  </div>
+              </div>
+            </div>
+          </div>
         </div>
     )
 }
